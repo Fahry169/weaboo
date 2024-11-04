7d51846fa2433724afddef6568ef68903895f3f8
fahry firdaus <ffahry169@gmail.com>
2024-11-04 09:24:46 +0700

    making episode icon and rating icon

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Browsers, Star } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,10 +10,10 @@ const AnimeList = ({ api }) => {
       <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-8 px-4 mb-8">
         {api.data?.map((data) => {
           return (
-            <div className="cursor-pointer hover:brightness-75">
+            <div className="cursor-pointer hover:brightness-75 transition-all">
               <Link
                 href={`/anime/${data.mal_id}`}
-                className="text-color-1 hover:text-black transition-all"
+                className="text-color-1 hover:text-black"
               >
                 <div className="relative aspect-[3/4]">
                   <Image
@@ -24,6 +27,22 @@ const AnimeList = ({ api }) => {
                 <h3 className="text-sm font-medium mt-2 line-clamp-1">
                   {data.title_english || data.title}
                 </h3>
+                <div className="justify-between flex items-center text-sm pt-1">
+                  <div className="flex gap-1 items-center">
+                    <Browsers 
+                    size={20}
+                    weight="bold"
+                    color="#2b2d42"
+                     />
+                    <p>{data.episodes || <span className="font-bold">-</span>}
+                    </p>
+                  </div>
+                  <div className="flex gap-0.5 items-center">
+                    <Star size={18} 
+                    weight="fill" color="#F3C623" />
+                    <p>{data.score || "-"}</p>
+                  </div>
+                </div>
               </Link>
             </div>
           );
