c5dd7ff518ad3b0760e039e9fabcd32de1b575e9
fahry firdaus <ffahry169@gmail.com>
2024-11-21 17:11:26 +0700

    banner (demo)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import AnimeList from "@/components/AnimeList";
 import TopAnime from "@/components/TopAnime";
+import Banner from "@/components/Utilities/Banner";
 import Header from "@/components/Utilities/Header";
 // import { getRandomAnime, reproduce } from "@/libs/api-libs";
 import { getAnimeResponse } from "@/libs/api-libs";
@@ -8,12 +9,18 @@ const Page = async () => {
   const airingAnime = await getAnimeResponse("seasons/now", "limit=10");
   const newAnime = await getAnimeResponse("seasons/upcoming", "limit=10");
   const topAnime = await getAnimeResponse("top/anime", "limit=10");
+  
 
   // let animeRandom = await getRandomAnime("recommendations/anime", "entry");
   // animeRandom = reproduce(animeRandom, 5);
 
   return (
     <>
+      <section>
+        <Banner/>
+      </section>
+
+
       <section>
         <Header
           title="Airing Anime"
@@ -41,7 +48,7 @@ const Page = async () => {
           TitleLink="View All"
           HrefLink="/populer"
         />
-        <TopAnime api={topAnime.data} />
+        <TopAnime api={topAnime} />
       </section>
 
       {/* anime rekomendasi */}
---
+++ src/components/Utilities/Banner/index.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+const Banner = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const bannerImages = [
+    "banner_op.png",
+    "banner_naruto.png",
+    "banner_bleach.png",
+  ];
+
+  const logoImages = [
+    "logo_op.png",
+    "logo_narutoo.png",
+    "logo_bleach.png",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % bannerImages.length);
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [bannerImages.length]);
+
+  const handleLeft = () => {
+    setCurrentIndex(
+      (currentIndex - 1 + bannerImages.length) % bannerImages.length
+    );
+  };
+
+  const handleRight = () => {
+    setCurrentIndex((currentIndex + 1) % bannerImages.length);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(/banner/${bannerImages[currentIndex]})`,
+      }}
+      className="w-full h-[650px] bg-cover bg-center bg-no-repeat transition-all duration-700 relative"
+    >
+      <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between px-4">
+        <button
+          onClick={handleLeft}
+          className="hover:scale-110 transition-all duration-100 hover:animate-pulse"
+        >
+          <ArrowLeft size={32} color="white" />
+        </button>
+        <button
+          onClick={handleRight}
+          className="hover:scale-110 transition-all duration-100 hover:animate-pulse"
+        >
+          <ArrowRight size={32} color="white" />
+        </button>
+      </div>
+      <div className="absolute left-10 md:left-16 lg:left-24 transition-all duration-500 ease-in-out top-1/2 -translate-y-1/2 md:translate-y-0">
+      <Image
+                unoptimized={false}
+                src={`/logo/${logoImages[currentIndex]}`}
+                alt={"test"}
+                width={500}
+                height={180}
+                className="w-max h-12 md:h-20 lg:h-32 hover:scale-105 transition-all duration-300"
+              />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
